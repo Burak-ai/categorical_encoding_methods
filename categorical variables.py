47480703a59241a8d547b168e72fd6182826d703
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-14 15:37:09 +0300

    Update categorical variables.py

--- categorical variables.py
+++ categorical variables.py
@@ -56,6 +56,7 @@ X_valid = X_valid_full[my_cols].copy()
 
 print(X_train.head())
 
+
 # Get list of categorical variables
 s = (X_train.dtypes == 'object')
 object_cols = list(s[s].index)
@@ -73,6 +74,8 @@ drop_X_train = X_train.select_dtypes(exclude=['object'])
 drop_X_valid = X_valid.select_dtypes(exclude=['object'])
 print("MAE from approach 1 (Drop Categorical variables):")
 print(score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+
+
 
 
 
